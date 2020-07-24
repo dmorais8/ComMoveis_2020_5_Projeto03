722425679b0770d5c82e11c10e6c3a01efea946d
David Morais <morais.david@live.com>
2020-07-23 21:01:37 -0300

    :tada: Release 1.0.2

--- metadata.py
+++ metadata.py
@@ -5,5 +5,5 @@ __copyright__ = "Copyright 2020, LTE-Advanced Calculator"
 __credits__ = ["David Morais"]
 __maintainer__ = "David Morais"
 __email__ = "moraisdavid8@gmail.com"
-__status__ = "Development"
-__version__ = "1.0.1-SNAPSHOT"
+__status__ = "Stable"
+__version__ = "1.0.2"
